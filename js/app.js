fd128ccda8602fbd6dcf025bfd5a833d1b1c7ce0
Julien Vernet <julien@vernet.me>
2015-09-15 14:47:07 +0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function ($) {
+
+	/**
+	 * YQL supports JSONP & CORS!
+	 * http://developer.yahoo.com/yql/
+	 * http://stackoverflow.com/a/8579158
+	 */
+
+	var projectSlug = 'awesome-support',
+		endpoint = 'https://www.transifex.com/_/userspace/ajax/widgets/languages/project/' + projectSlug + '/?all=1';
+
+	if (sessionStorage.getItem(projectSlug + '-translations')) {
+
+		renderTranslations();
+
+	} else {
+
+		$.getJSON('http://query.yahooapis.com/v1/public/yql', {
+				q: 'select * from json where url=\"' + endpoint + '&fmt=JSON\"',
+				format: 'json'
+			},
+			function (data) {
+				if (data.query.results) {
+					sessionStorage.setItem(projectSlug + '-translations', JSON.stringify(data.query.results.json));
+					renderTranslations();
+				} else {
+					alert('Something went wrong. Please try again.');
+				}
+			}
+		);
+
+	}
+
+	function renderTranslations() {
+
+		var json = $.parseJSON(sessionStorage.getItem(projectSlug + '-translations')),
+			getTemplate = $('#languages-template').html(),
+			template = Handlebars.compile(getTemplate);
+
+		// Render the table
+		$('#language-list').html(template(json));
+
+		// Count translations
+		$('#languages-count').text(json.languages.length);
+	}
+
+});
